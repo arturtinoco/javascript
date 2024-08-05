18146a7570be12cfc03b4a2312b1b83b3b32b309
arturtinoco <arturguz.tinoco@gmail.com>
2024-08-04 21:48:03 -0300

    ex014 concluido
    Temas para cada turno do dia e a respectiva hora

--- aula12-exercicios/ex014/scripts/script.js
+++ aula12-exercicios/ex014/scripts/script.js
@@ -1,1 +1,22 @@
-alert('olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora < 12) {
+        // Bom dia
+        img.src = 'imagens/manha-250.png'
+        document.body.style.background = '#e9c06b'
+    } else if (hora >= 12 && hora <= 18) {
+        //Boa tarde
+        img.src = 'imagens/tarde-250.png'
+        document.body.style.background = '#925b60'
+    } else {
+        // Boa noite
+        img.src = 'imagens/noite-250.png'
+        document.body.style.background = '#323e41'
+    }
+}
