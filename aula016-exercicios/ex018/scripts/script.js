0c6b502f69233c1d304f62fc8136f7433ac6373a
arturtinoco <arturguz.tinoco@gmail.com>
2024-08-06 02:12:15 -0300

    Ajustes e otimizações do ex018

--- aula016-exercicios/ex018/scripts/script.js
+++ aula016-exercicios/ex018/scripts/script.js
@@ -1,10 +1,10 @@
-let numList = []
+let inum = document.getElementById('inum')
+let list = document.querySelector('select#result')
 let res = document.querySelector('div#analysis')
+let numList = []
 
 function addNumber(){
     res.innerHTML = ''
-    let inum = document.getElementById('inum')
-    let list = document.querySelector('select#result')
     let num = Number(inum.value)
 
     if (num.length == 0 || num <= 0 || num > 100 || numList.includes(num)) {
@@ -14,35 +14,40 @@ function addNumber(){
         numList.push(num)
         let item = document.createElement('option')
         item.text = `Valor ${num} adicionado.`
-        item.value = num
+        //item.value = num
         list.appendChild(item)
         inum.value = ''
+        inum.focus()
     }
     
 }
 
 function analyse() {
-    inum.value = ''
-    numList.sort(function(a, b){return a - b})
-    let amountInside = numList.length
-    let highestNum = numList[amountInside - 1]
-    let lowestNum = numList[0]
-    let total = 0
+    if (numList.length == 0) {
+        alert('[ERRO] Adicione valores antes de finalizar!')
+    } else {
+        inum.value = ''
+        numList.sort(function(a, b){return a - b})
 
-    for (let i = 0; i < amountInside; i++) {
-        total += numList[i]
+        let amountInside = numList.length
+        let highestNum = numList[amountInside - 1]
+        let lowestNum = numList[0]
+        let total = 0
+
+        for (let i = 0; i < amountInside; i++) {
+            total += numList[i]
+        }
+
+        let median = total / amountInside
+
+        res.innerHTML = `
+                        <p>Ao todo, temos ${amountInside} números cadastrados.</p>
+                        <p>O maior valor informado foi ${highestNum}.</p>
+                        <p>O menor valor informado foi ${lowestNum}.</p>
+                        <p>Somando todos os valores, temos ${total}</p>
+                        <p>A média dos valores digitados é ${+parseFloat((median).toFixed(2))}</p>
+                        `
     }
-
-    let median = total / amountInside
-
-    
-    res.innerHTML = `
-                    <p>Ao todo, temos ${amountInside} números cadastrados.</p>
-                    <p>O maior valor informado foi ${highestNum}.</p>
-                    <p>O menor valor informado foi ${lowestNum}.</p>
-                    <p>Somando todos os valores, temos ${total}</p>
-                    <p>A média dos valores digitados é ${+parseFloat((median).toFixed(2))}</p>
-                    `
 }
 
 
